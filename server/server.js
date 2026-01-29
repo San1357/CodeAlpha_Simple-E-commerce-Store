8d7bf98a9977d18de9996c47503ff9f7201e37e4
Aayush Gupta <sanayush1357@gmail.com>
2026-01-29 22:35:41 +0530

    chore(server): integrate order routes with express app

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/cart", require("./routes/cartRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
 
 // Health check
 app.get("/", (req, res) => {
